ebcfd0d5a8823ecd937b67f954de891d213fcba2
Angelo Morada <angelomorada@gmail.com>
2023-04-17 16:45:38 +0800

    added active record validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+  validates :movie_title, presence: true
+  validates :blurb, presence: true
+  validates :date_released, presence: true
+  validates :origin_country, presence: true
+  validates :showing_start, presence: true
+  validates :showing_end, presence: true
 end
